c407944dc37bd5686ae14da18c0337ccc8c3357b
Dominik <dominikjaniga91@gmail.com>
2020-04-23 19:10:47 +0200

    Add import static to FlightControllerTest

--- src/test/java/spaceflight/controller/FlightControllerTest.java
+++ src/test/java/spaceflight/controller/FlightControllerTest.java
@@ -1,5 +1,6 @@
 package spaceflight.controller;
 
+import org.mockito.Mockito;
 import spaceflight.model.Flight;
 import spaceflight.service.FlightServiceImpl;
 import spaceflight.service.PassengerServiceImpl;
@@ -14,8 +15,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -41,30 +42,34 @@ public class FlightControllerTest {
         Flight flight2 = new Flight("Jupiter", LocalDate.of(2020,3,25), LocalDate.of(2020,3,25), 15, 30000.0);
         Flight flight3 = new Flight("Mars", LocalDate.of(2020,3,25), LocalDate.of(2020,3,25), 15, 20000.0);
         Flight flight4 = new Flight("Jupiter", LocalDate.of(2020,3,25), LocalDate.of(2020,3,25), 15, 30000.0);
+
         BDDMockito.given(flightService.findAll()).willReturn(List.of(flight1, flight2, flight3, flight4));
     }
 
     @Test
     void shouldReturnSizeOfFlightsList_afterRequestingRightPathToController() throws Exception {
-            mockMvc.perform(MockMvcRequestBuilders.get("/allFlights")
-                    .contentType(MediaType.APPLICATION_JSON))
-                    .andExpect(MockMvcResultMatchers.status().isOk())
-                    .andExpect(MockMvcResultMatchers.jsonPath("$.*", Matchers.hasSize(4)));
+        mockMvc.perform(get("/flights")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", Matchers.hasSize(4)));
+
+        BDDMockito.verify(flightService, Mockito.times(1)).findAll();
+        BDDMockito.verifyNoMoreInteractions(flightService);
 
     }
 
     @Test
     void shouldReturnFlightsListAsJson_afterRequestingRightPathToController() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/allFlights")
+        mockMvc.perform(get("/flights")
                 .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$[0].destination", Matchers.is("Moon")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[1].ticketPrice", Matchers.is(30000.0)))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[2].startDate", Matchers.is("2020-03-25")))
-                .andExpect(MockMvcResultMatchers.jsonPath("$[3].destination", Matchers.is("Jupiter")));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].destination", Matchers.is("Moon")))
+                .andExpect(jsonPath("$[1].ticketPrice", Matchers.is(30000.0)))
+                .andExpect(jsonPath("$[2].startDate", Matchers.is("2020-03-25")))
+                .andExpect(jsonPath("$[3].destination", Matchers.is("Jupiter")));
+
+        BDDMockito.verify(flightService, Mockito.times(1)).findAll();
+        BDDMockito.verifyNoMoreInteractions(flightService);
     }
-
-
-
 
 }
