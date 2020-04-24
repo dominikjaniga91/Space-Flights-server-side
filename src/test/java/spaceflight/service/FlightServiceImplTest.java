ec550a998ce7f301947eeb2c0eb417f2334c5586
Dominik <dominikjaniga91@gmail.com>
2020-04-24 22:57:52 +0200

    Set up first database integration test:
    Test valid number of object save to H2 DB
    Create new properties file to set up
    test environment.

---
+++ src/test/java/spaceflight/service/FlightServiceImplTest.java
@@ -0,0 +1,49 @@
+package spaceflight.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import spaceflight.model.Flight;
+import spaceflight.repository.FlightRepositoryImpl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class FlightServiceImplTest {
+
+    @Autowired
+    FlightRepositoryImpl flightRepository;
+
+    @BeforeAll
+    void setUpData(){
+
+        List<Flight> flights = Stream.of(new Flight("Moon", LocalDate.of(2020,3,25), LocalDate.of(2020,4,25), 15, 15000.0),
+                new Flight("Jupiter", LocalDate.of(2020,3,25), LocalDate.of(2020,6,25), 15, 30000.0),
+                new Flight("Mars", LocalDate.of(2020,3,25), LocalDate.of(2020,4,25), 15, 20000.0),
+                new Flight("Jupiter", LocalDate.of(2020,3,25), LocalDate.of(2020,4,25), 15, 30000.0)).collect(Collectors.toList());
+
+        flights.forEach(flight -> flightRepository.save(flight));
+    }
+
+
+    @Test
+    void shouldReturnFourFlights_afterGetAllFlightsFromDatabase(){
+        Assertions.assertEquals(4, flightRepository.findAll().size());
+    }
+
+
+}
