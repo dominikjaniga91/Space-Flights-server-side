d6da531885c7e934077adcca1d793f1b48958ac4
Dominik <dominikjaniga91@gmail.com>
2020-04-28 13:01:57 +0200

    Add successfull authentication method to LoginFilter

--- src/main/java/spaceflight/authentication/filter/LoginFilter.java
+++ src/main/java/spaceflight/authentication/filter/LoginFilter.java
@@ -7,7 +7,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import spaceflight.authentication.AuthenticationService;
 import spaceflight.authentication.model.AccountCredentials;
+
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -35,5 +38,12 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
         );
     }
 
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) {
 
+        AuthenticationService.addToken(response, authResult.getName());
+    }
 }
