4a19b99ba6195af15f4571c084ed306c0aca387b
Dominik <dominikjaniga91@gmail.com>
2020-04-28 12:55:07 +0200

    Create attemptAuthentication method in LoginFilter class

---
+++ src/main/java/spaceflight/authentication/filter/LoginFilter.java
@@ -0,0 +1,39 @@
+package spaceflight.authentication.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import spaceflight.authentication.model.AccountCredentials;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class LoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    public LoginFilter(String url, AuthenticationManager authenticationManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authenticationManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+        AccountCredentials credentials = new ObjectMapper()
+                .readValue(request.getInputStream(), AccountCredentials.class);
+
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        credentials.getUsername(),
+                        credentials.getPassword(),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+
+}
