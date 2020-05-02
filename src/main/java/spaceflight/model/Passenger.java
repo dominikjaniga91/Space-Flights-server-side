e400a0ab321624292c9d1430c0421c18dafd0dec
Dominik <dominikjaniga91@gmail.com>
2020-05-02 17:19:01 +0200

    Add additional field validation for Passenger entity.

--- src/main/java/spaceflight/model/Passenger.java
+++ src/main/java/spaceflight/model/Passenger.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.*;
@@ -19,11 +20,11 @@ public class Passenger {
     private Integer id;
 
     @Column(name="first_name")
-    @NotNull
+    @NotBlank(message = "First name cannot be empty")
     private String firstName;
 
     @Column(name="last_name")
-    @NotNull
+    @NotBlank(message = "Last name cannot be empty")
     private String lastName;
 
     @Column(name="sex")
@@ -31,7 +32,7 @@ public class Passenger {
     private String sex;
 
     @Column(name="country")
-    @NotNull
+    @NotBlank(message = "Country cannot be empty")
     private String country;
 
     @Column(name="notes")
@@ -40,7 +41,6 @@ public class Passenger {
     @Column(name="birth_date")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @JsonSerialize(using = LocalDateSerializer.class)
-    @NotNull
     private LocalDate birthDate;
 
     @JsonIgnore
