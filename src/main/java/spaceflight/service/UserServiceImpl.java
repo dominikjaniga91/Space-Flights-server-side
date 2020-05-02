424750fdd27b9961e2674944f8edba8bbc8fa0aa
Dominik <dominikjaniga91@gmail.com>
2020-05-02 22:22:23 +0200

    Add password encoding functionality in save user method.

--- src/main/java/spaceflight/service/UserServiceImpl.java
+++ src/main/java/spaceflight/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package spaceflight.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import spaceflight.model.User;
 import spaceflight.repository.UserRepositoryImpl;
@@ -11,14 +12,17 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private UserRepositoryImpl userRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepositoryImpl userRepository) {
+    public UserServiceImpl(UserRepositoryImpl userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public User saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
