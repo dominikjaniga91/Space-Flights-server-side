f3cc9409db1492b9a75c0dd9df9a75b4043fbf8e
Dominik <dominikjaniga91@gmail.com>
2020-05-04 18:55:09 +0200

    Set up custom database table name for user entity

--- src/main/java/spaceflight/model/User.java
+++ src/main/java/spaceflight/model/User.java
@@ -6,15 +6,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
 import java.util.Collections;
 
 @Entity
+@Table(name = "users")
 public class User implements UserDetails {
 
     @Id
