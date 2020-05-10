dc772bdcf1fd6e265adbf78ab3881c5a40302224
Dominik <dominikjaniga91@gmail.com>
2020-05-10 16:00:27 +0200

    Add method (and test) that save data to xlsx file

--- src/main/java/spaceflight/service/fileservice/SaveExcelService.java
+++ src/main/java/spaceflight/service/fileservice/SaveExcelService.java
@@ -1,7 +1,14 @@
 package spaceflight.service.fileservice;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SaveExcelService {
@@ -13,4 +20,34 @@ public class SaveExcelService {
         return  spreadsheet;
     }
 
+    public XSSFWorkbook saveDataToFile(List<Map<String, Object>> elements) {
+
+        XSSFWorkbook spreadsheet = createNewXlsxFile("Users");
+        XSSFSheet sheet = spreadsheet.getSheetAt(0);
+        int rowNumber = sheet.getLastRowNum() + 1;
+        for (Map<String, Object> element : elements) {
+
+            Row row = sheet.createRow(rowNumber++);
+            int cellNumber = 0;
+            for (Map.Entry<String, Object> object : element.entrySet()) {
+
+                Cell cell = row.createCell(cellNumber++);
+                Object obj = object.getValue();
+                if (obj instanceof Integer) {
+                    cell.setCellValue((Integer) obj);
+                } else if (obj instanceof String) {
+                    cell.setCellValue((String) obj);
+                } else if (obj instanceof Long) {
+                    cell.setCellValue((Long) obj);
+                } else if (obj instanceof Double) {
+                    cell.setCellValue((Double) obj);
+                } else if (obj instanceof Date) {
+                    cell.setCellValue((Date) obj);
+                } else if (obj instanceof Boolean) {
+                    cell.setCellValue((Boolean) obj);
+                }
+            }
+        }
+        return spreadsheet;
+    }
 }
--- src/test/java/spaceflight/service/fileservice/SaveExcelServiceTest.java
+++ src/test/java/spaceflight/service/fileservice/SaveExcelServiceTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 public class SaveExcelServiceTest {
 
@@ -24,6 +28,25 @@ public class SaveExcelServiceTest {
         XSSFWorkbook spreadsheet = saveExcelService.createNewXlsxFile("Users");
         Assertions.assertEquals("Users", spreadsheet.getSheetName(0));
 
+    }
+
+    @Test
+    @DisplayName("Should return number of rows after save data to xlsx file")
+    void shouldReturnRightNumberOfRowsANdColumns_afterSaveDataToXlsxFile(){
+        LinkedHashMap<String, Object> user1 = new LinkedHashMap<>();
+        user1.put("firstName","Dominik");
+        user1.put("lastName","Janiga");
+
+        LinkedHashMap<String, Object> user2 = new LinkedHashMap<>();
+        user2.put("firstName","Jan");
+        user2.put("lastName","Kowalski");
+
+        List<Map<String, Object>> users = List.of(user1, user2);
+        XSSFWorkbook spreadsheet = saveExcelService.saveDataToFile(users);
+
+        int rows = spreadsheet.getSheetAt(0).getLastRowNum();
+
+        Assertions.assertEquals(2, rows);
     }
 
 }
