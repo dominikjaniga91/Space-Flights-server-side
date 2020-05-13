dd37b0536b722e503869b204cf38f6457a09e8cd
Dominik <dominikjaniga91@gmail.com>
2020-05-13 18:43:22 +0200

    Add get mapping for passengers in pdf controller

--- src/main/java/spaceflight/controller/PdfFileController.java
+++ src/main/java/spaceflight/controller/PdfFileController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import spaceflight.service.fileservice.SavePdfFile;
 import spaceflight.service.implementation.FlightServiceImpl;
+import spaceflight.service.implementation.PassengerServiceImpl;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -22,18 +23,21 @@ public class PdfFileController {
 
     private SavePdfFile pdfService;
     private FlightServiceImpl flightService;
+    private PassengerServiceImpl passengerService;
     private Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @Autowired
     public PdfFileController(SavePdfFile pdfService,
-                          FlightServiceImpl flightService) {
+                            FlightServiceImpl flightService,
+                            PassengerServiceImpl passengerService) {
 
         this.pdfService = pdfService;
         this.flightService = flightService;
+        this.passengerService = passengerService;
     }
 
     @GetMapping("/flights")
-    public void generatePdfFile(HttpServletResponse response){
+    public void generateFlightsPdfFile(HttpServletResponse response){
 
         List<Map<String,Object>> elements = flightService.getFlightsAsListOfMaps();
         Document document = new Document();
@@ -41,6 +45,21 @@ public class PdfFileController {
             response.setContentType("application/pdf");
             PdfWriter.getInstance(document, outStream);
             pdfService.saveDateToPdfFile(elements, document);
+            outStream.flush();
+        }catch (IOException | DocumentException ex){
+            logger.error(ex.getMessage());
+        }
+    }
+
+    @GetMapping("/passengers")
+    public void generatePassengersPdfFile(HttpServletResponse response){
+
+        List<Map<String,Object>> passengers = passengerService.getPassengersAsListOfMaps();
+        Document document = new Document();
+        try(OutputStream outStream = response.getOutputStream()){
+            response.setContentType("application/pdf");
+            PdfWriter.getInstance(document, outStream);
+            pdfService.saveDateToPdfFile(passengers, document);
             outStream.flush();
         }catch (IOException | DocumentException ex){
             logger.error(ex.getMessage());
